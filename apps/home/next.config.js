7f2631150a230936287bd85fb1b993fd6531fcce
davidnguyen179 <dzungnguyen179@gmail.com>
2021-05-10 21:01:19 +0900

    first commit

---
+++ apps/home/next.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  // disable default icon of nextjs at a bottom of page
+  devIndicators: {
+    autoPrerender: false,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  pageExtensions: ["tsx"],
+  pwa: {
+    dest: "public",
+  },
+};
